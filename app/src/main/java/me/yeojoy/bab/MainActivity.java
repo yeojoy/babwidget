4e051225e0ed68a77473b6bb868312b88640d4df
yeojoy <yeojoy.kim@gmail.com>
2015-02-10 09:23:47 +0900

    갤넥에서 위젯 나옴. minWidth때문인 듯. 홈화면 텍스트 크기 변경.

--- app/src/main/java/me/yeojoy/bab/MainActivity.java
+++ app/src/main/java/me/yeojoy/bab/MainActivity.java
@@ -1,9 +1,15 @@
 package me.yeojoy.bab;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -40,34 +46,55 @@ public class MainActivity extends ActionBarActivity
         mTvResult.setText("");
         if (menus != null) {
 
-            StringBuilder sb = new StringBuilder();
-            sb.append(menus.getDate()).append("\n");
-
+            int d = (int) getResources().getDisplayMetrics().density;
+            SpannableStringBuilder ssb = new SpannableStringBuilder();
+            SpannableString ss = new SpannableString(menus.getDate());
+            
+            ss.setSpan(new AbsoluteSizeSpan(20 * d), 0, ss.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            ssb.append(ss).append("\n\n");
+            
             
             Set<String> otherMenus = menus.getOtherMenus();
+            
             if (otherMenus != null) {
-                sb.append("오늘의 점심 메뉴").append("\n");
-                sb.append("Main : ").append(menus.getMainMenu()).append("\n");
-                sb.append(menus.getSubMenuFirst()).append("\n");
-                sb.append(menus.getSubMenuSecond()).append("\n");
-                sb.append(menus.getSubMenuThird()).append("\n");
-                sb.append(menus.getSubMenuFourth()).append("\n");
+                ss = new SpannableString("오늘의 점심 메뉴");
+                ss.setSpan(new AbsoluteSizeSpan(26 * d), 0, ss.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                ss.setSpan(new StyleSpan(Typeface.BOLD), 0, ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                ssb.append(ss).append("\n");
+                
+                ss = new SpannableString(menus.getMainMenu());
+                ss.setSpan(new AbsoluteSizeSpan(20 * d), 0, ss.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                ssb.append(ss).append("\n");
+
+                ss = new SpannableString(menus.getSubMenuFirst());
+                ss.setSpan(new AbsoluteSizeSpan(16 * d), 0, ss.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                ssb.append(ss).append("\n");
+                ssb.append(menus.getSubMenuSecond()).append("\n");
+                ssb.append(menus.getSubMenuThird()).append("\n");
+                ssb.append(menus.getSubMenuFourth()).append("\n");
                 
                 for (String s : otherMenus) {
-                    sb.append(s).append("\n");
+                    ssb.append(s).append("\n");
                 }
             }
 
             Set<String> dieteticInfo = menus.getDieteticInfo();
             if (dieteticInfo != null) {
-                sb.append("\n").append("영양정보").append("\n");
+                ss = new SpannableString("영양정보");
+                ss.setSpan(new AbsoluteSizeSpan(26 * d), 0, ss.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                ss.setSpan(new StyleSpan(Typeface.BOLD), 0, ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                ssb.append("\n").append(ss);
 
+                ss = new SpannableString("");
+                ss.setSpan(new AbsoluteSizeSpan(16 * d), 0, ss.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                ssb.append(ss).append("\n");
+                
                 for (String s : dieteticInfo) {
-                    sb.append(s).append("\n");
+                    ssb.append(s).append("\n");
                 }
             }
 
-            mTvResult.setText(sb);
+            mTvResult.setText(ssb);
         } else {
             mTvResult.setText("등록된 메뉴가 없습니다.");
         }
